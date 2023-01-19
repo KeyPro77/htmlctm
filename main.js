4236fbeced0e34ccca79f6760b0a2cced9bc79c5
KeyPro77 <93216062+KeyPro77@users.noreply.github.com>
2023-01-19 22:23:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,23 @@ var actWidth = $("#tile-1 .nav-tabs").find(".active").parent("li").width();
 var actPosition = $("#tile-1 .nav-tabs .active").position();
 $("#tile-1 .slider").css({"left":+ actPosition.left,"width": actWidth});
 
+
+
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+
+    if (username === "stay" && password === "high") {
+        location.href = "premium.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
